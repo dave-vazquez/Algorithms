51362ac34c9d4b6e4b6bf20af67beb348a816ab6
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-18 18:25:08 -0400

    recipe_batches.py solution

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python
 
 import math
+import os
+
+os.system("clear")
+
 
 def recipe_batches(recipe, ingredients):
-  pass 
+    if len(ingredients) != len(recipe):
+        return 0
+
+    ing_count = []
+
+    for rec, ing in zip(recipe.items(), ingredients.items()):
+        ing_count.append(ing[1] // rec[1])
+
+    return min(ing_count)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
