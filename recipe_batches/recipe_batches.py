a44d6f463a3dfecfcfe6754db1150bc5166b4ffd
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-19 10:48:11 -0400

    adds RTC and justification

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -5,6 +5,38 @@ import os
 
 os.system("clear")
 
+'''
+RUN-TIME COMPLEXITY: O(n)
+
+Justification:
+    Lines 43 - 46 are single operations 
+        constituting  a RTC of  O(1)
+    
+    Line 49 - The loop itself has a RTC of O(n) 
+        or O(n + m) where n and m represent two input sizes
+        which can be reduced to O(n) as n+m constitute the 
+        summation of both input sizes.
+    Line 49 - The zip() function has a RTC of O(1)
+            in Python 2 it would be O(n) --> (builds a new list)
+            in Python 3 it would be O(n) --> (allocates an iterable 
+                and assings a parameter array to an internal field)
+            ref: https://stackoverflow.com/questions/36877715/what-is-the-time-complexity-of-zip-in-python
+        both dict.items() in the loop have a RTC of O(1)
+            in Python 2 it would be O(n) --> (builds a new list)
+            in Python 3, it's O(1) -- (does not build a new list)
+            ref: https://stackoverflow.com/questions/39219065/what-is-the-time-complexity-of-dict-keys-in-python
+    Line 52 - append() has a RTC of O(1)
+        ref: https://wiki.python.org/moin/TimeComplexity
+    Line 59 - min() has an RTC of O(n)
+        ref: https://wiki.python.org/moin/TimeComplexity
+
+    Ignoring all O(1) RTC's, you are left with:
+        O(n) for the loop, and O(n) for finding the min
+        giving us O(n+n), or O(2n) which can be reduced 
+        down to O(n)
+
+'''
+
 
 def recipe_batches(recipe, ingredients):
     # check for equal sets of ingredients
