24e8c8b52e1c2da7267adbd34d516e7d90210487
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-19 12:42:07 -0400

    eating_cookies.py adds resource link

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python
 
 import sys
+import os
 
+os.system("clear")
 # The cache parameter is here for if you want to implement
-# a solution that is more efficient than the naive 
+# a solution that is more efficient than the naive
 # recursive solution
+
+# TODO READ: https://medium.com/i-math/combinations-permutations-fa7ac680f0ac
+
+
 def eating_cookies(n, cache=None):
-  pass
+    pass
+
 
 if __name__ == "__main__":
-  if len(sys.argv) > 1:
-    num_cookies = int(sys.argv[1])
-    print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
-  else:
-    print('Usage: eating_cookies.py [num_cookies]')
+    if len(sys.argv) > 1:
+        num_cookies = int(sys.argv[1])
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(
+            ways=eating_cookies(num_cookies), n=num_cookies))
+    else:
+        print('Usage: eating_cookies.py [num_cookies]')
